3b8fc97fb24e8aaf1431d4cc776f4494ec1814bb
dethghost <admin@deth.org.ua>
2018-03-15 21:24:28 +0200

    Initial commit

---
+++ CustomArrayList/MyArrayList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+
+namespace CustomArrayList
+{
+    public class MyArrayList : IEnumerable
+    {
+        private Object[] array;
+
+        public object this[int index]
+        {
+            get { return this.array[index]; }
+            set
+            {
+                if (index <= this.array.Length - 1)
+                {
+                    this.array[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public MyArrayList(Object[] array = null)
+        {
+            if (array == null)
+            {
+                this.array = new Object[1];
+            }
+
+            this.array = array;
+        }
+
+        public int Length
+        {
+            get { return this.array.Length; }
+        }
+
+        public void Add(Object o)
+        {
+            if (this.array[this.array.Length - 1] == null)
+            {
+                this[this.array.Length - 1] = o;
+            }
+            else
+            {
+                IncreaseArray();
+                this[this.array.Length - 1] = o;
+            }
+        }
+
+        public void Remove(int index)
+        {
+            if (this.array.Length - 1 < index)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var o = new Object[this.array.Length - 1];
+            int oLengthCounter = 0;
+            for (int i = 0; i < this.array.Length; i++)
+            {
+                if (index == i)
+                {
+                    continue;
+                }
+
+                o[oLengthCounter++] = this.array[i];
+            }
+
+            this.array = o;
+        }
+
+        private void IncreaseArray()
+        {
+            Object[] newArray = new Object[this.array.Length + 1];
+            this.array.CopyTo(newArray, 0);
+            this.array = newArray;
+        }
+
+        public void Insert(int index, Object value)
+        {
+            if (index > this.array.Length - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var o = new Object[this.array.Length + 1];
+            int oCounter = 0;
+            for (int i = 0; i < index; i++)
+            {
+                o[i] = this[i];
+            }
+
+            o[index] = value;
+            for (int i = index; i < this.array.Length; i++)
+            {
+                o[i + 1] = this[i];
+            }
+
+            this.array = o;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < this.array.Length; i++)
+            {
+                yield return this.array[i];
+            }
+        }
+    }
+}
---
+++ CustomArrayList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CustomArrayList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            MyArrayList arrayList = new MyArrayList(new Object[]{10, 20, "string"});
+            
+            arrayList.Add("New Object");
+            Console.WriteLine(arrayList[3]);
+            foreach (var VARIABLE in arrayList)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            
+            arrayList.Remove(1);
+            foreach (var VARIABLE in arrayList)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+
+            Console.WriteLine(arrayList.Length);
+            arrayList.Insert(2, "New Value in 3rd position");
+            foreach (var VARIABLE in arrayList)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            Console.ReadLine();
+        }
+    }
+}
